f2635f1d89314d784238a5a3d166974652027e3b
HannaNikola <111384616+HannaNikola@users.noreply.github.com>
2026-03-29 23:05:01 +0200

    fix

--- src/index.ts
+++ src/index.ts
@@ -95,7 +95,7 @@ const corsOptions: cors.CorsOptions = {
 };
 
 app.use(cors(corsOptions));
-app.options("*", cors(corsOptions));
+app.options("/{*path}", cors(corsOptions));
 
 app.use(express.json());
 
